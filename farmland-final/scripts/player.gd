355f51f906268d577ae426cd35ea6c6ebd1019c1
sdas321 <sdas@students.mags.school.nz>
2026-06-10 09:29:43 +1200

    added Physics layers
    i also edited the player movement after adding the physics layers

--- farmland-final/scripts/player.gd
+++ farmland-final/scripts/player.gd
@@ -1,23 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var SPEED: float = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	
-	# Handle jump.
+	
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2(0.0, 0.0)
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	
+	velocity = SPEED * direction.normalized()
+	
+	
 
 	move_and_slide()
