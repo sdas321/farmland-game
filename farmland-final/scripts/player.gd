7e0a3e457bbf351a981272b981690f49617b792a
sdas321 <sdas@students.mags.school.nz>
2026-06-14 23:57:08 +1200

    Added inventory

--- farmland-final/scripts/player.gd
+++ farmland-final/scripts/player.gd
@@ -1,24 +1,12 @@
 extends CharacterBody2D
-
-
 var SPEED: float = 200.0
-const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
+	pass
 	
-	
-	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2(0.0, 0.0)
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
-	
 	velocity = SPEED * direction.normalized()
-	
-	
-
 	move_and_slide()
