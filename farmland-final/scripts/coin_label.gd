93d1dfdfaabd8b2d37ebacfc275a2d5b40ad58cb
sdas321 <sdas@students.mags.school.nz>
2026-08-12 09:37:12 +1200

    tutorial

--- farmland-final/scripts/coin_label.gd
+++ farmland-final/scripts/coin_label.gd
@@ -4,5 +4,5 @@ func _ready() -> void:
 	Global.coins_changed.connect(self._on_coins_changed)
 	text = str(Global.coins) 
 
-func _on_coins_changed(new_amount: int) -> void:
-	text = str(new_amount)
+func _on_coins_changed(coins: int) -> void:
+	text = "Coins:" + str(coins)
--- farmland-final/scripts/coin_market_label.gd
+++ farmland-final/scripts/coin_market_label.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 	Global.coins_changed.connect(self._on_coins_updated)
 	text = "Coins: " + str(Global.coins)
 
-func _on_coins_updated(new_amount: int) -> void:
-	text = "Coins: " + str(new_amount)
+func _on_coins_updated(coins: int) -> void:
+	text = "Coins: " + str(coins)
