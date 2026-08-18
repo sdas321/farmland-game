c8c251f8275cac9282f9a858ccbdbc0c238829f9
sdas321 <sdas@students.mags.school.nz>
2026-08-19 11:37:51 +1200

    sound

--- farmland-final/scripts/main_menu.gd
+++ farmland-final/scripts/main_menu.gd
@@ -3,15 +3,15 @@ var button_press= AudioStreamPlayer
 
 
 func _on_play_pressed() -> void:
-	$button_press.play()
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_how_to_play_pressed() -> void:
-	$button_press.play()
+	$AudioStreamPlayer.play()
 	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
 
 
 func _on_quit_pressed() -> void:
-	$button_press.play()
+	$AudioStreamPlayer.play()
 	get_tree().quit()
